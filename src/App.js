1daf11e612f1bc3d10789ad17347e27e997787ef
YGI240 <145881086+YGI240@users.noreply.github.com>
2024-04-01 11:16:02 -0700

    Second commit NavBar creation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './pages/NavBar';
-import Home from './pages/Home';
+import NavBar from './pages/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import About from'./pages/About/About';
 
 const App = () => {
   return (
     <>
     <NavBar/>
-    <Home/>
+   {/* <Home/>
+    <About/>
+  */}
 
     </>
   );
-}
+  }
 
 export default App;
