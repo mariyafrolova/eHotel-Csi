11c764a31c797b476cbf9736c72d8b177491709b
YGI240 <145881086+YGI240@users.noreply.github.com>
2024-04-01 12:45:47 -0700

    Home page header

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import './navbar.css'
+import { RiHotelLine } from "react-icons/ri";;
 
 const NavBar = () => {
     return(
-        <div>NavBar</div>
+        <section className='navBarSection'>
+            <div className='header'>
+                <div className='logoDiv'>
+                    <a href='a' className='logo'>
+                       <h1 className='flex'> <RiHotelLine className='icon'/> 
+                       Jillian
+                       </h1> 
+                       
+                    </a>
+                </div>
+
+                <div className='navBar'>
+                 <ul className='navlists flex'>
+                    <li className='navItem'>
+                    <a href='#' className='navLink'> Home </a>
+                    </li>
+                    <li className='navItem'>
+                    <a href='#' className='navLink'> About </a>
+                    </li>
+                    <li className='navItem'>
+                    <a href='#' className='navLink'> Reservations </a>
+                    </li>
+                    <li className='navItem'>
+                    <a href='#' className='navLink'> Login </a>
+                    </li>
+                 
+                 </ul>
+                </div>
+            </div>
+        </section>
         )
     }
 export default NavBar
