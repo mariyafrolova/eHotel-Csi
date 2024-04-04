cb309b673e710c82fac217e3596af009376e7c1d
YGI240 <145881086+YGI240@users.noreply.github.com>
2024-04-03 17:15:05 -0700

    Home Card with search requierements

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -14,8 +14,43 @@ const Home = () => {
                     </p>
                     
                 </div>
+
+                <div className='homeCard grid'>
                 
-                
+                    <div className='locationDiv'>
+                        <label htmlFor='location'>Location</label>
+                        <input type='text' placeholder='Location'/>
+                    </div>
+
+                    <div className='dateDiv'>
+                        <label htmlFor='date'>Date</label>
+                        <input type='text' placeholder='Date'/>
+                    </div>
+
+                    <div className='priceDiv'>
+                        <label htmlFor='price'> Price </label>
+                        <input type='text' placeholder='Price range'/>
+                    </div>
+
+                    <div className='hotelDiv'>
+                        <label htmlFor='hotel'>Hôtel</label>
+                        <input type='text' placeholder='Hôtel'/>
+                    </div>
+
+                    <div className='adultsDiv'>
+                        <label htmlFor='number'>Adults</label>
+                        <input type='text' placeholder='Number of Adults'/>
+                    </div>
+
+                    <div className='kidsDiv'>
+                        <label htmlFor='number'>Children</label>
+                        <input type='text' placeholder='Number of children'/>
+                    </div>
+                    
+                    <button className='btn'>
+                        Search
+                    </button>
+                </div>
             </div>
         </section>
         )
